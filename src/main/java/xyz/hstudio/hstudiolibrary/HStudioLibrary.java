465ef658660730ab1169a78fae4cc777c358dc08
MrCraftGoo <2044154518@qq.com>
2020-02-15 12:35:22 +0800

    添加bStats

--- src/main/java/xyz/hstudio/hstudiolibrary/HStudioLibrary.java
+++ src/main/java/xyz/hstudio/hstudiolibrary/HStudioLibrary.java
@@ -1,10 +1,12 @@
 package xyz.hstudio.hstudiolibrary;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HStudioLibrary extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        new Metrics(this, 6502);
     }
 }
