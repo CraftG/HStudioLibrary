4544aa81a39c9eaeaa4968db2334a3977fe6f599
MrCraftGoo <2044154518@qq.com>
2020-02-15 13:41:19 +0800

    修复Json读取的bug

--- src/main/java/xyz/hstudio/hstudiolibrary/json/JsonUtils.java
+++ src/main/java/xyz/hstudio/hstudiolibrary/json/JsonUtils.java
@@ -1,5 +1,7 @@
 package xyz.hstudio.hstudiolibrary.json;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.bukkit.plugin.Plugin;
@@ -12,11 +14,19 @@ import java.lang.reflect.Field;
 
 public class JsonUtils {
 
+    /**
+     * 自动加载Json文件
+     *
+     * @param plugin   插件实例
+     * @param instance Json类实例
+     * @param file     Json文件
+     */
     public static <T> T load(final Plugin plugin, final T instance, final File file) {
         if (!file.exists()) {
             throw new IllegalStateException("文件 " + file.getName() + " 不存在！");
         }
         try {
+            Gson gson = new Gson();
             JsonObject element = new JsonParser().parse(new FileReader(file)).getAsJsonObject();
             // 遍历所有成员
             for (Field field : instance.getClass().getDeclaredFields()) {
@@ -27,18 +37,18 @@ public class JsonUtils {
                     if (annotation == null) {
                         continue;
                     }
-                    String[] paths = annotation.path().split(".");
+                    String[] paths = annotation.path().contains(".") ? annotation.path().split("\\.") : new String[]{annotation.path()};
 
-                    Object value = null;
+                    JsonElement value = null;
                     for (String path : paths) {
-                        value = element.getAsJsonObject(path);
+                        value = value == null ? element.get(path) : value.getAsJsonObject().get(path);
                     }
 
                     if (value == null) {
                         throw new IllegalStateException("没有找到文件 " + file.getName() + " 值 " + field.getName() + " ！");
                     }
 
-                    field.set(instance, value);
+                    field.set(instance, gson.fromJson(value, field.getType()));
                 } catch (IllegalAccessException e) {
                     throw new IllegalStateException("加载文件 " + file.getName() + " 值 " + field.getName() + " 时出现错误！");
                 } finally {
